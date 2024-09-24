da44aa801530cd9156af6142c210bb24cdb1dee3
Stephie <sdemaude@student.42lehavre.fr>
2024-09-24 17:26:48 +0200

    add of async function for data handling

--- bot.py
+++ bot.py
@@ -6,33 +6,77 @@ import time
 from websockets.asyncio.client import connect
 
 
-async def hello():
-    uri = "ws://localhost:8765"
-    async with connect(uri) as websocket:
-        name = "BOT"
+class Coord:
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
 
-        await websocket.send(name)
-        print(f">>> {name}")
+class Data:
+	bot_paddle_pos = Coord(0, 0)
+	adv_paddle_pos = Coord(0, 0)
+	ball_speed = Coord(0, 0)
+	ball_pos = Coord(0, 0)
 
-        message = await websocket.recv()
-        print(f"<<< {message}")
+class Bot:
+	name = "BOT"
+	opponent = ""
+	play_side = 0
+	bot_view = Data()
+	real_data = Data()
 
-        if message == "start":
-            while True:
-                get = await websocket.recv()
-                #print(f"<<< {get}")
+	def calculateNextMove(self):
+		if self.bot_view.ball_pos.y < 500:
+			return "up"
+		else:
+			return "down"
 
-                message = get.split(":")
-                if message[0] == "youare":
-                    player = int(message[1])
 
-                if message[0] == "pos":
-                    if int(message[-1]) <= 500:
-                        await websocket.send("up")
-                    elif int(message[-1]) > 500:
-                        await websocket.send("down")
-                #await asyncio.sleep(1)
-                
+bot = Bot()
+
+async def updateData(data):
+	global bot
+	if data[0] == "pos":
+		bot.real_data.ball_pos.x = int(data[1])
+		bot.real_data.ball_pos.y = int(data[2])
+	elif data[0] == "mov":
+		bot.real_data.ball_speed.x = int(data[1])
+		bot.real_data.ball_speed.y = int(data[2])
+	elif data[0] == bot.opponent:
+		bot.real_data.adv_paddle_pos.x = int(data[1])
+		bot.real_data.adv_paddle_pos.y = int(data[2])
+	elif data[0] == bot.name:
+		bot.real_data.bot_paddle_pos.x = int(data[1])
+		bot.real_data.bot_paddle_pos.y = int(data[2])
+
+
+async def getMessage(websocket):
+	message = await websocket.recv()
+	data = message.split(":")
+	updateData(data)
+
+
+async def main():
+	uri = "ws://localhost:8765"
+	async with connect(uri) as websocket:
+
+		await websocket.send(bot.name)
+		message = await websocket.recv()
+
+		if message == "start":
+			message = await websocket.recv()
+			data = message.split(":")
+			if data[0] == "youare":
+				bot.play_side = int(data[1])
+			message = await websocket.recv()
+			data = message.split(":")
+			if data[0] == "opponent":
+				bot.opponent = data[1]
+
+			task = asyncio.create_task(getMessage(websocket))
+			while True:
+				next_move = bot.calculateNextMove()
+				await websocket.send(next_move)
+
 
 if __name__ == "__main__":
-    asyncio.run(hello())
+    asyncio.run(main())
